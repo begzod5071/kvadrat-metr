fab108ef9649edbbb174cf4cf700490f5bad2b33
UmarAli <umaralimuminjonov@gmail.com>
2021-11-24 16:58:14 +0500

    first commit

--- controllers/projectCtrl.js
+++ controllers/projectCtrl.js
@@ -12,6 +12,39 @@ const projectCtrl = {
   },
   createProject: async (req, res) => {
     try {
+      const { title } = req.body;
+
+      const newProject = new Project({
+        title,
+      });
+      await newProject.save();
+
+      res.json({ msg: "Created project" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  updateProject: async (req, res) => {
+    try {
+      const { title } = req.body;
+
+      
+
+
+      res.json({ msg: "Updated project" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  createProject: async (req, res) => {
+    try {
+      const { title } = req.body;
+
+      const newProject = new Project({
+        title,
+      });
+      await newProject.save();
+
       res.json({ msg: "Created project" });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
