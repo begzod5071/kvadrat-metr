f5a8ed3db80f678df9d1bd815b5f2d2c2b6befb0
UmarAli <umaralimuminjonov@gmail.com>
2021-11-25 09:30:45 +0500

    added project schema

--- models/projectModel.js
+++ models/projectModel.js
@@ -2,12 +2,100 @@ const mongoose = require("mongoose");
 
 const projectSchema = new mongoose.Schema(
   {
-    title: {
+    name: {
       type: String,
       required: true,
       trim: true,
     },
-
+    floor: {
+      from: {
+        type: Number,
+        default: 1,
+      },
+      to: {
+        type: Number,
+        required: true,
+      },
+    },
+    area: {
+      from: {
+        type: Number,
+        default: 10,
+      },
+      to: {
+        type: Number,
+        required: true,
+      },
+    },
+    rooms: {
+      from: {
+        type: Number,
+        default: 1,
+      },
+      to: {
+        type: Number,
+        required: true,
+      },
+    },
+    repair: {
+      type: Boolean,
+      default: false,
+    },
+    parking: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    location: {
+      address: {
+        type: String,
+        required: true,
+      },
+      landmark: {
+        type: String,
+        required: true,
+      },
+      map: {
+        type: String,
+        required: true,
+      },
+    },
+    images: {
+      type: [
+        {
+          url: {
+            type: String,
+            required: true,
+          },
+          public_id: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+      default: [],
+    },
+    info: {
+      uz: {
+        type: String,
+        required: true,
+      },
+      ru: {
+        type: String,
+        default: null,
+      },
+      en: {
+        type: String,
+        default: null,
+      },
+    },
   },
   {
     timestamps: true,
