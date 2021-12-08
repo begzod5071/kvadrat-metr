a0f6250b928016fc8b1bc9387f99fc5fc6d6e3f3
UmarAli <umaralimuminjonov@gmail.com>
2021-12-08 10:17:57 +0500

    completed filter endpoint

--- server/controllers/filterCtrl.ts
+++ server/controllers/filterCtrl.ts
@@ -10,27 +10,36 @@ const filterCtrl = {
     try {
       const { room, district, developerId, priceFrom, priceTo } = req.query;
 
-      console.log(district);
       const quiries = { ...req.query };
 
-      const projects = await Project.find(
-        district
-          ? {
-              "location.district": { $in: district },
-            }
-          : {}
-      );
+      const replacements: any = {
+        district: "location.district",
+        room: "rooms",
+        developerId: "developerId",
+      };
 
-      // await Promise.all(
-      //   projects.map(async (project) => {
-      //     const apartments = await Apartment.find(
-      //       !room
-      //         ? { projectId: project._id }
-      //         : { projectId: project._id, room }
-      //     );
-      //   })
-      // );
-      //
+      let replacedItems = Object.keys(quiries).map((key) => {
+        if (key === "district" || key === "developerId") {
+          const newKey = replacements[key] || key;
+          return { [newKey]: { ["$in"]: quiries[key] } };
+        }
+
+        if (key === "rooms") {
+          const newKey = replacements[key] || key;
+          return {
+            [`${newKey}.from`]: { ["$lte"]: quiries[key] },
+            [`${newKey}.to`]: { ["$gte"]: quiries[key] },
+          };
+        }
+      });
+      let newTab: any = {};
+
+      if (replacedItems.length !== 0) {
+        newTab = replacedItems.reduce((a, b) => Object.assign({}, a, b));
+      }
+
+      const projects = await Project.find(newTab);
+
       res.json({ length: projects.length, projects });
     } catch (err: any) {
       return res.error.handleError(res, err);
