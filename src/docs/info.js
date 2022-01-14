6166a846fe50ddd02cd23964daedc7af4430ffc7
begzod5071 <rafirafikovbegzod5071@gmail.com>
2022-01-14 15:02:05 +0500

    started file for docs

---
+++ src/docs/info.js
@@ -0,0 +1,14 @@
+module.exports = {
+  openapi: "3.0.3",
+  info: {
+    title: "Mohirdev Auth API",
+    description:
+      "API for registration and payment system for mohirdev's basic and practicum",
+    version: "1.0.0",
+    contact: {
+      name: "UmarAli",
+      email: "umaralimuminjonov@gmail.com",
+      url: "https://umarali.herokuapp.com",
+    },
+  },
+};
